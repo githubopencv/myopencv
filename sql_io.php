22ed3efc80ce5c5118434a77bae077217c4b6616
tparks5 <tparks5@mail.csuchico.edu>
2014-10-06 11:21:59 -0700

    php won't do functions, copy paste working example and use that

--- sql_io.php
+++ sql_io.php
@@ -1,13 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
 <?php
-
-
-
+/*
 function sqlconnect($sqld, $user, $pw, $db_name)
 {
 	$con = mysqli_connect($sqld, $user, $pw, $db_name);
 
 	if( mysqli_connect_errno() )
-		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		echo "<p>Failed to connect to MySQL: </p>" . mysqli_connect_error();
+	else
+		echo "<p>connection established: </p>" . $con
+
 	return $con;
 } //end sqlconnect
 
@@ -16,6 +22,21 @@ function sqlclose($con)
 	mysqli_close($con);
 
 }
+*/
+echo "<p>attempting connection </p>";
+//sqlconnect(127.0.0.1, root, "mysql", "test");
 
+function hi($name)
+{
+	$s "<p>hi $name</p>";
+	return $s;
+}
 
+//$t =  hi("dude");
+
+echo "<p>test " . hi("dude") . "</p>";
 ?>
+
+</body>
+</html>
+
