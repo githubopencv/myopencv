0872ebc2a80ebf8a0dd84c654cabf5fd814b054a
tparks5 <tparks5@mail.csuchico.edu>
2014-10-19 19:26:02 -0700

    initial attempt at converting between php and mysql time formats for file transfer dates

--- file_upload.php
+++ file_upload.php
@@ -15,17 +15,35 @@ padding:5px;
 <center>
 <?php
 
-    $connection = new mysqli('localhost','root','mysql','fileDB');
-  if(!$connection)
+  //there should be a non-root user that oversees all fileDB operations
+  $connection = new mysqli('localhost','root','mysql','fileDB');
+  
+  if(!$connection) //print error and quit if no connection
   {
-    echo('<p> Unable to connect. </p>');
+    echo('<p> Unable to connect to database. </p>');
 	  exit();
   }
-  
-  $fileName$_POST['file'];
-  
-  $result = mysqli_query($connection, "INSERT INTO group1 (username , post_time, post_date, file) values ();");
-  
+  else //else proceed with upload
+  {
+    //ensure date and time are configured correctly
+    date_default_timezone_set('America/Los_Angeles');
+    
+    $username = "default";
+    $fileName = $_POST['file'];
+    /*
+    php uses linux epoch time, mysql does not. it is necessary to transform between them.
+    
+    transform date from php format to mysql format: 
+    $mysqldate = date('Y-m-d H:i:s', $somedate); //$somedate arg optional
+    
+    transform date from mysql format to php format:
+    $phpdate = strtotime( $mysqldate );
+    */
+    $postDateTime = date('Y-m-d H:i:s'); 
+    
+    $result = mysqli_query($connection, "INSERT INTO group1 (username , post_time, post_date, file) values ('$filename', '$postDateTime' );");
+    
+  }
 
 ?>
 
