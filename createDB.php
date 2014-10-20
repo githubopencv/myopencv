aca5fe28c00dd178d8a7ecd70e13955065ddc692
tparks5 <tparks5@mail.csuchico.edu>
2014-10-19 21:23:20 -0700

    fixed syntax error in db events and calendar tables

--- createDB.php
+++ createDB.php
@@ -11,8 +11,8 @@
 
 	echo "Database setup commenced\n";
 
-	$con = new mysqli('127.0.0.1', 'root');  
-	mysqli_query($con, 'use mysql;');
+	$connection = new mysqli('127.0.0.1', 'root');  
+	mysqli_query($connection, 'use mysql;');
 	
 	/*
 	every user's login data is stored in loginDB.login
@@ -22,35 +22,79 @@
 	loginIP is the last IP from which a user logged in from.
 	sessionID is checking that a user's current login is valid.
 	*/
-	mysqli_query($con, 'create database db;');
-	mysqli_query($con, 'create table db.logins (username varchar(255), password varchar(255), email text, loggedIn boolean, loginIP varchar(255), sessionID text);');
+	$result = mysqli_query($connection, 'create database db;');
+	if (!$result)
+  {
+    $err = mysqli_error($connection);
+    echo($err . "\n");
+  }
+  else
+    echo "created database db\n";
 	
-	echo "created loginDB\n";
+	$result = mysqli_query($connection, 'create table db.logins (username varchar(255), password varchar(255), email text, loggedIn boolean, loginIP varchar(255), sessionID text);');
+	if (!$result)
+  {
+    $err = mysqli_error($connection);
+    echo($err . "\n");
+  }
+  else
+	  echo "created login table\n";
 
-  /*
-  Each group has its own table of members.
-  do something like SELECT $username FROM *  
-  */
-	//mysqli_query($con, 'create database groupMemberDB;');
-	mysqli_query($con, 'create table db.groupMember (username varchar(255), groupname varchar(255), admin boolean);');
-  echo "created groupMembersDB\n";
 
-	//mysqli_query($con, 'create database fileDB;');
-	mysqli_query($con, 'create table db.files (username varchar(255), groupname varchar(255), filename text, postDateTime datetime, file mediumblob);');
-  echo "created filesDB\n";
+	//mysqli_query($connection, 'create database groupMemberDB;');
+	$result = mysqli_query($connection, 'create table db.groupMembers (username varchar(255), groupname varchar(255), admin boolean);');
+  if (!$result)
+  {
+    $err = mysqli_error($connection);
+    echo($err . "\n");
+  }
+  else
+    echo "created groupMembers table\n";
 
-	//mysqli_query($con, 'create database eventDB;');
-	mysqli_query($con, 'create table db.events (username varchar(255), eventname varchar(255), description text, timeStart time, timeStop time, dateStart date, dateStop date, recurrence set);');
-  echo "created eventsDB\n";
+	//mysqli_query($connection, 'create database fileDB;');
+	$result = mysqli_query($connection, 'create table db.files (username varchar(255), groupname varchar(255), filename text, postDateTime datetime, file mediumblob);');
+  if (!$result)
+  {
+    $err = mysqli_error($connection);
+    echo($err . "\n");
+  }
+  else
+    echo "created files table\n";
+
+	//mysqli_query($connection, 'create database eventDB;');
+	$result = mysqli_query($connection, "create table db.events (username varchar(255), 
+	eventName varchar(255), 
+	description text, 
+	dateStart datetime, 
+	dateStop datetime, 
+	recurrence set('Mondays', 'Tuesdays', 'Wednesdays', 'Thursdays', 'Fridays', 'Saturdays', 'Sundays', 'Weekly', 'Monthly', 'Yearly') );");
+  if (!$result)
+  {
+    $err = mysqli_error($connection);
+    echo($err . "\n");
+  }
+  else
+    echo "created events table\n";
   
-	//mysqli_query($con, 'create database calendarDB;');
-	mysqli_query($con, 'create table db.calendar (eventName varchar(255), groupName varchar(255), description text, timeStart time, timeStop time, dateStart date, dateStop date, recurrence set);');
-  echo "created calendarDB\n";
+	//mysqli_query($connection, 'create database calendarDB;');
+	$result = mysqli_query($connection, "create table db.calendar (eventName varchar(255), 
+	groupName varchar(255), 
+	description text,
+	dateStart datetime, 
+	dateStop datetime, 
+	recurrence set('Mondays', 'Tuesdays', 'Wednesdays', 'Thursdays', 'Fridays', 'Saturdays', 'Sundays', 'Weekly', 'Monthly', 'Yearly'));");
+  if (!$result)
+  {
+    $err = mysqli_error($connection);
+    echo($err . "\n");
+  }
+  else 
+    echo "created calendar table\n";
 
-  //mysqli_query($con, 'insert into login (user, pw, loggedIn, loginIP) in loginDB values ('root', 'toor', 'true', '127.0.0.1'); ');
+  //mysqli_query($connection, 'insert into login (user, pw, loggedIn, loginIP) in loginDB values ('root', 'toor', 'true', '127.0.0.1'); ');
 	//echo "loginDB insert query completed";
 /*	
-	$result = mysqli_query($con, 'SELECT * FROM group1 in groupMembersDB;');
+	$result = mysqli_query($connection, 'SELECT * FROM group1 in groupMembersDB;');
   echo "loginDB select query completed\n";
 
 	while ($row = mysqli_fetch_array($result) )
@@ -59,18 +103,18 @@
 	}
 */
 /*
-	if(!$con) //mysqli_connect_errno() )
+	if(!$connection) //mysqli_connect_errno() )
 	{
 		echo "ERR: could not connect to MySQL: " . mysqli_connect_error() . "\n";
 	}
 	else
 	{
-		echo "Connection Established $con\n";
+		echo "Connection Established $connection\n";
 	}
 */
 	echo "Database setup completed\n";
 
-//	return $con;
+//	return $connection;
 //}
 
 //echo "sql connection test \n";
--- file_upload.php
+++ file_upload.php
@@ -15,8 +15,8 @@ padding:5px;
 <center>
 <?php
 
-  //there should be a non-root user that oversees all fileDB operations
-  $connection = new mysqli('localhost','root','mysql','fileDB');
+  //there should be a non-root user that oversees all file DB operations
+  $connection = new mysqli('localhost','root','mysql','db');
   
   if(!$connection) //print error and quit if no connection
   {
@@ -28,20 +28,15 @@ padding:5px;
     //ensure date and time are configured correctly
     date_default_timezone_set('America/Los_Angeles');
     
-    $username = "default";
-    $fileName = $_POST['file'];
-    /*
-    php uses linux epoch time, mysql does not. it is necessary to transform between them.
+    //checks should be run to ensure these are valid names,
+    //and that this isn't a duplicate database entry
+    $username = "defaultUser";
+    $groupname = "defaultGroup";
+    $postDateTime = date('Y-m-d H:i:s'); //mysql's date format 
+    $fileName = $_FILE['uploadFile']['name'];
+    $file = $_FILE['uploadFile'];
     
-    transform date from php format to mysql format: 
-    $mysqldate = date('Y-m-d H:i:s', $somedate); //$somedate arg optional
-    
-    transform date from mysql format to php format:
-    $phpdate = strtotime( $mysqldate );
-    */
-    $postDateTime = date('Y-m-d H:i:s'); 
-    
-    $result = mysqli_query($connection, "INSERT INTO group1 (username , post_time, post_date, file) values ('$filename', '$postDateTime' );");
+    $result = mysqli_query($connection, "INSERT INTO db.files (username, groupname, filename, postDateTime, file) values ('$username', '$groupname', '$filename', '$postDateTime', '$file', );");
     
   }
 
