700d43c50d6abd93680ea738b9daca20dec2e389
tparks5 <tparks5@mail.csuchico.edu>
2014-10-29 12:39:14 -0700

    updated nukeDB for our server database

--- nukeDB.php
+++ nukeDB.php
@@ -7,31 +7,31 @@
   anyone could type <our website>.com/nukeDB.php into firefox,
   and brick our project. Only run from cmd with "php nukeDB.php"
   */
+	$db = "jvillanueva";
+	echo "Dropping tables\n";
 
-	echo "Nuking database from orbit...\n";
-
-	$con = new mysqli('127.0.0.1', 'root'); 
-	mysqli_query($con, 'use mysql;'); 
+	$con = new mysqli("db.ecst.csuchico.edu", "jvillanueva", "H5msvMGK", "jvillanueva", "5551"); //using db, not $db, on purpose 
+	mysqli_query($con, "use $db;"); 
 	
-	mysqli_query($con, 'drop table db.login');
+	mysqli_query($con, "drop table $db.login");
 	echo "dropped table login\n";
 
-	mysqli_query($con, 'drop table db.groupMembers;');
+	mysqli_query($con, "drop table $db.groupMembers;");
 	echo "dropped table groupMembers\n";
 
-	mysqli_query($con, 'drop table db.files;');
+	mysqli_query($con, "drop table $db.files;");
   echo "dropped table files\n";
 
-	mysqli_query($con, 'drop database db.events;');
+	mysqli_query($con, "drop database $db.events;");
 	echo "dropped table events\n";
   
-	mysqli_query($con, 'drop database db.calendar;');
+	mysqli_query($con, "drop database $db.calendar;");
 	echo "dropped table calendar\n";
 
-  mysqli_query($con, 'drop database db;');
-	echo "dropped database db\n";
+	// mysqli_query($con, "drop database $db;");
+ 	//echo "dropped database $db\n";
 
-	echo "Databases nuked\n";
+	echo "Dropped tables\n";
 
 ?>
 
