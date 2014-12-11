9ac908f40ba3ede90e49eadb16e35253e6b2b7b9
sphagura1 <saviphagura@gmail.com>
2014-12-10 22:23:20 -0800

    Update log_out.php
    Redirects to login page

--- final-1.0/log_out.php
+++ final-1.0/log_out.php
@@ -9,26 +9,27 @@ session_destroy();
 setcookie (session_name(), '', time()-300);
 ?>
 <html>
-<head>
-<title> Log Out </title>
-<link rel="stylesheet" type="text/css" href="login_style.css">
-</head>
-<style>
-header {
-background-color:lightgray;
-font: 12px "Helvetica Neue", Helvetica, Arial, sans-serif;
-color: #888;
-text-align:center;
-padding:5px;
-}
-</style>
+	<head>
+	<title> Log Out </title>
+	<link rel="stylesheet" type="text/css" href="login_style.css">
+	</head>
+	<style>
+		header {
+			background-color:lightgray;
+			font: 12px "Helvetica Neue", Helvetica, Arial, sans-serif;
+			color: #888;
+			text-align:center;
+			padding:5px;
+		}
+	</style>
 <body>
 <header>
-<h2>Thanks for Visiting the Group Management Project!</h2>
+<!--<h2>Thanks for Visiting the Group Management Project!</h2>-->
 </header>
 <center>
 <body id="body_color">
-<p>You have successfully logged out</p>
+<!--<p>You have successfully logged out</p>-->
+<META HTTP-EQUIV="refresh" CONTENT="0;URL=index.php">
 </center>
 </body>
 </html>
